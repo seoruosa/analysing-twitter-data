6914ce15ecedaa1a3a872f49c94e83cb3e6a86b3
Thiago <thiago.giachetto@gmail.com>
2019-09-17 23:48:14 -0300

    create first api call

---
+++ util/connecting-with-api.py
@@ -0,0 +1,11 @@
+from util.__connection__ import *
+import tweepy
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
